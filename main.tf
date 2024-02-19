0b59a3a4e00e5dd95602438756ddf47190f276a6
“mnandhakumarrock” <mnandhakumarrock@gmail.com>
2024-02-19 09:11:37 +0000

    1st commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "myTASK1"
+  }
+}
+
+resource "aws_subnet" "pubsub" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "TASK1 pub-sub"
+  }
+}
