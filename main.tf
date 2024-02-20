178a7d4e19bcc41861d05199857c7d1e62613952
mnandhakumarrock <118954824+mnandhakumarrock@users.noreply.github.com>
2024-02-20 18:13:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,11 @@ resource "aws_subnet" "pubsub" {
     Name = "TASK1 pub-sub"
   }
 }
+resource "aws_subnet" "pvtsub" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "pvt-sub"
+  }
+}
